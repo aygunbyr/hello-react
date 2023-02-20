62bc7f28f6df3137bacbfe13b6823b45d832cbba
Aygün Bayır <turkoaygun@gmail.com>
2023-02-20 23:15:49 +0300

    Data fetching hata yakalama işlendi

--- src/components/Users.js
+++ src/components/Users.js
@@ -9,36 +9,42 @@ function Users() {
   useEffect(() => {
     // axios("https://jsonplaceholder.typicode.com/users")
     //   .then((res) => setUsers(res.data))
+    //   .catch((error) => {
+    //     console.log("error", error);
+    //   })
     //   .finally(() => setLoading(false));
     getData();
   }, []);
 
   const getData = async () => {
-    const { data: users } = await axios(
-      "https://jsonplaceholder.typicode.com/users"
-    );
-    const { data: posts } = await axios(
-      `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
-    );
+    try {
+      const { data: users } = await axios(
+        "https://jsonplaceholder.typicode.com/users"
+      );
+      const { data: posts } = await axios(
+        `https://jsonplaceholder.typicode.com/posts?userId=${users[0].id}`
+      );
 
-    setLoading(false);
-    setUsers(users);
-    setPosts(posts);
+      setLoading(false);
+      setUsers(users);
+      setPosts(posts);
+    } catch (error) {
+      console.log("error", error);
+    }
   };
 
   return (
     <div>
+      {loading && <div>Yükleniyor...</div>}
 
-        {loading && <div>Yükleniyor...</div>}
-      
-        <h2>Users</h2>
-        <ul>
+      <h2>Users</h2>
+      <ul>
         {users.map((user) => (
           <li key={user.id}>{user.name}</li>
         ))}
       </ul>
       <h2>Posts</h2>
-        <ul>
+      <ul>
         {posts.map((post) => (
           <li key={post.id}>{post.title}</li>
         ))}
